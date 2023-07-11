365be0dde817689a218fec24c41cc4fe1d6c3b93
Arielle Gironza <ariellegironza@Arielles-Air.fios-router.home>
2023-07-10 23:07:22 -0400

    updated schema

--- models/symptoms.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('./connection');
-
-const symptomSchema = new mongoose.Schema({
-    name: String
-});
-
-const Symptoms = mongoose.model('symptom', symptomSchema);
-
-module.exports = Symptoms;
